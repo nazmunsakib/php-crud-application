6f5dcdc6579439ad1974d8df81b23a236dcd35e9
Nazmun Sakib <wp.coder3@gmail.comm>
2021-06-15 20:23:00 +0600

    Finish crud project

--- inc/functions.php
+++ inc/functions.php
@@ -4,29 +4,34 @@ define("DB_NAME", "C:/xampp/htdocs/PHP-projects/php-crud/DB/db.txt");
 function seed(){
     $data = [
         [
+            "id" => 1,
             "name" => "Nazmun Sakib",
             "class" => 12,
-            "roll" => "01",
+            "roll" => "1",
         ],
         [
+            "id" => 2,
             "name" => "Bill Grets",
             "class" => 12,
-            "roll" => "02",
+            "roll" => "2",
         ],
         [
+            "id" => 3,
             "name" => "Mark Jukerbarg",
             "class" => 12,
-            "roll" => "03",
+            "roll" => "3",
         ],
         [
+            "id" => 4,
             "name" => "Elon Maks",
             "class" => 12,
-            "roll" => "04",
+            "roll" => "4",
         ],
         [
+            "id" => 5,
             "name" => "Einnstine",
             "class" => 12,
-            "roll" => "05",
+            "roll" => "5",
         ],
     ];
     $serializeData = serialize($data);
@@ -39,6 +44,7 @@ function generateReport(){
     ?>
     <table>
         <tr>
+            <th>Id</th>
             <th>Name</th>
             <th>Class</th>
             <th>Roll</th>
@@ -46,12 +52,92 @@ function generateReport(){
         </tr>
         <?php foreach($students as $student): ?>
             <tr>
+                <td><?php printf("%s", $student['id'] ); ?> :</td>
                 <td><?php printf("%s", $student['name'] ); ?></td>
                 <td><?php printf("%s", $student['class'] ); ?></td>
                 <td><?php printf("%s", $student['roll'] ); ?></td>
-                <td><?php printf("<a href='index.php?task=edit&id=%s'>Edit</a> | <a href='index.php?task=delete&id=%s'>Delete</a>", $student['roll'], $student['roll']); ?></th>
+                <td><?php printf("<a href='index.php?task=edit&id=%s'>Edit</a> | <a href='index.php?task=delete&id=%s' class='delete' >Delete</a>", $student['id'], $student['id']); ?></th>
             </tr>
         <?php endforeach; ?> 
     </table>
     <?php
+}
+
+function addStudent($name, $class, $roll){
+    $found = false;
+    $serializeData = file_get_contents(DB_NAME);
+    $students = unserialize($serializeData);
+    foreach($students as $_student){
+        if($_student['roll'] == $roll){
+            $found = true;
+            break;
+        }
+    }
+    if(!$found){
+        $student = [
+            "id" => getNewId($students),
+            "name" => $name,
+            "class" => $class,
+            "roll" => $roll,
+        ];
+        array_push($students, $student);
+        $addSerializeData = serialize($students);
+        file_put_contents(DB_NAME, $addSerializeData, LOCK_EX );
+        return true;
+    }
+    return false;
+}
+
+function getStudent($id){
+    $serializeData = file_get_contents(DB_NAME);
+    $students = unserialize($serializeData);
+    foreach($students as $student){
+        if( $student['id'] == $id ){
+            return $student;
+        }
+    }
+    return false;
+}
+
+function updateStudent($id, $name, $class, $roll){
+    $found = false;
+    $serializeData = file_get_contents(DB_NAME);
+    $students = unserialize($serializeData);
+    foreach($students as $student){
+        if($student['roll'] == $roll && $student['id'] != $id){
+            $found = true;
+        }
+    }
+    if(!$found){
+        $students[$id -1]['name'] = $name;
+        $students[$id -1]['class'] = $class;
+        $students[$id -1]['roll'] = $roll;
+        $updateSerializeData = serialize($students);
+        file_put_contents(DB_NAME, $updateSerializeData, LOCK_EX );
+        return true;
+    }
+    return false;
+
+}
+function test(){
+    $serializeData = file_get_contents(DB_NAME);
+    $students = unserialize($serializeData);
+    print_r($students);
+}
+
+function deleteStudent($id){
+    $serializeData = file_get_contents(DB_NAME);
+    $students = unserialize($serializeData);
+    foreach($students as $offset=>$student){
+        if( $student['id'] == $id ){
+            unset($students[$offset]);
+        }
+    }
+    $serializedData               = serialize( $students );
+	file_put_contents( DB_NAME, $serializedData, LOCK_EX );
+}
+
+function getNewId($studemts){
+    $maxId = max(array_column($studemts, 'id'));
+    return $maxId+1;
 }
--- index.php
+++ index.php
@@ -4,9 +4,46 @@
     require_once 'inc/functions.php';
     $info = "";
     $task = $_GET['task']??'report';
+    $error = '0';
     if('seed'==$task){
         seed();
         $info = "Seeding is complete";
+    }
+    $name = '';
+    $class = '';
+    $roll = '';
+    if(isset($_POST['submit'])){
+        $name = filter_input( INPUT_POST, 'name', FILTER_SANITIZE_STRING );
+        $class = filter_input( INPUT_POST, 'class', FILTER_SANITIZE_STRING );
+        $roll = filter_input( INPUT_POST, 'roll', FILTER_SANITIZE_STRING );
+        $id = filter_input( INPUT_POST, 'id', FILTER_SANITIZE_STRING );
+        if($id){
+            if($id !='' && $name !='' && $class !='' && $roll !=''){
+            $updateStudent = updateStudent($id, $name, $class, $roll);
+            header('location: index.php?task=report');
+            if($updateStudent){
+                header('location: index.php?task=report');
+               }else{
+                    $error = '404'; 
+               }
+             }
+        }else{
+            if($name !='' && $class !='' && $roll !=''){
+                $studentSubmit = addStudent($name, $class, $roll);
+                if($studentSubmit){
+                 header('location: index.php?task=report');
+                }else{
+                     $error = '404'; 
+                }
+             }
+        }
+    }
+    if('delete'==$task){
+        $id = filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
+        if($id>0){
+            deleteStudent($id);
+            header('location: index.php?task=report');
+        }
     }
 ?>
 <head>
@@ -49,7 +86,59 @@
                 </div>
             </div>
         <?php endif; ?>
+        <?php if('add'==$task):?>
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <h2>Add A New Student</h2>
+                    <?php if('404'==$error): ?>
+                        <blockquote>
+                            The <?php echo $roll; ?> Roll numder is already exist to Another One
+                        </blockquote>
+                    <?php endif; ?>
+                    <form action="index.php?task=add" method="POST">
+                        <label for="name">Your Name</label>
+                        <input type="text" name="name" id="name" value="<?php echo $name; ?>">
+                        <label for="class">Your Class</label>
+                        <input type="number" name="class" id="" value="<?php echo $class; ?>" >
+                        <label for="roll">Your Roll</label>
+                        <input type="number" name="roll" id="" value="<?php echo $roll; ?>">
+                        <button type="submit" name="submit" class="button button-primary">SUBMIT</button>
+                    </form>
+                </div>
+            </div>
+        <?php endif; ?>
+        <?php 
+            if('edit'==$task):
+            $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+             $student = getStudent($id);
+             if($student):
+            ?>
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <h2>Update Student</h2>
+                    <?php if('404'==$error): ?>
+                        <blockquote>
+                            The <?php echo $roll; ?> Roll numder is already exist to Another One
+                        </blockquote>
+                    <?php endif; ?>
+                    <form method="POST">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>" >
+                        <label for="name">Your Name</label>
+                        <input type="text" name="name" id="name" value="<?php echo $student['name']; ?>">
+                        <label for="class">Your Class</label>
+                        <input type="number" name="class" id="" value="<?php echo $student['class']; ?>" >
+                        <label for="roll">Your Roll</label>
+                        <input type="number" name="roll" id="" value="<?php echo $student['roll']; ?>">
+                        <button type="submit" name="submit" class="button button-primary">UPDATE</button>
+                    </form>
+                </div>
+            </div>
+        <?php 
+            endif; 
+        endif; 
+        ?>
 
     </div>
+    <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
